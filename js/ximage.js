3359c5e389d5edaed28286d62ea25832c48f893c
Barton Phillips <bartonphillips@gmail.com>
2017-12-23 06:30:42 -0800

    modified:   css/applitec.css -- add xmas 	modified:   css/blp.css -- add xmas 	modified:   js/ximage.js -- make ajax use object

--- js/ximage.js
+++ js/ximage.js
@@ -11,16 +11,26 @@ var bannerImages = new Array, binx = 0;
 
 /* Called from 'index.php' */
 
-function dobanner(path, recursive, seq) {
+// dobanner()
+// path is a pattern to glob on.
+// obj: {size: size, recursive: yes|no, mode: seq|rand}
+
+function dobanner(path, obj) {
+  // obj has three members: size, recursive, mode.
+  
+  let recursive = obj.recursive;
+  let size = obj.size;
+  let mode = obj.mode;
+  
   $.ajax({
     url: 'http://www.bartonphillips.dyndns.org:8080/glob.proxy.php',
     type: 'get',
-    data: {path: path, recursive: recursive},
+    data: {path: path, recursive: recursive, size: size, mode: mode},
     success: function(data) {
-      console.log("data", data);
+      //console.log("data", data);
       bannerImages = data.split("\n");
       $("#show").html("<h3 class='center'>" + path + "</h3><img>");
-      bannershow(seq);
+      bannershow(obj.mode); // pass mode to bannershow()
     },
     error: function(err) {
       console.log("Error: ", err);
@@ -34,24 +44,20 @@ var m = new MersenneTwister();
 // Called from above. It displayes the image in "#show" and then sets a
 // timer and does it again and again.
 
-function bannershow(seq) {
-  if(seq == "seq") {
-    if(binx++ > (bannerImages.length - 1)) {
-      binx = 0;
-    }
-  } else {
-    binx = Math.floor(m.random() * bannerImages.length);
+function bannershow() {
+  if(binx++ > (bannerImages.length - 1)) {
+    binx = 0;
   }
-  
+    
   var image = new Image;
   image.src = bannerImages[binx];
   $(image).load(function() {
     $("#show img").attr('src', image.src);
-    setTimeout(function() { bannershow(seq); }, 5000);
+    setTimeout(function() { bannershow(); }, 5000);
   });
 
   $(image).error(function(err) {
     console.log(err);
-    setTimeout(function() { bannershow(seq); }, 5000);
+    setTimeout(function() { bannershow(); }, 5000);
   });
 }
