21d70c7fc3a5909e3e30a41abb6e3593a0e4c7aa
Barton Phillips <bartonphillips@gmail.com>
2023-01-16 15:36:44 -0500

    Updated myfingerprints.php with new fingers. Removed myfingerprints.json, not used any more. For info on htmx.js and htmx.min.js see bartonphillips.com/test-examples/htmx.html. Upgraded jquery et all to latest version. 	new file:   js/htmx.js 	new file:   js/htmx.min.js 	renamed:    js/jquery-3.3.1.js -> js/jquery-3.6.3.js 	renamed:    js/jquery-migrate-3.3.2.js -> js/jquery-migrate-3.4.0.js 	deleted:    myfingerprints.json 	modified:   myfingerprints.php

--- myfingerprints.php
+++ myfingerprints.php
@@ -3,14 +3,16 @@
 // I 'require_once()'.
 
 return [
-        'hFBzuVRDeIWdbhXmhZv7' => "HP",
-        'e30hJHxUeaToTAB6g4Zv' => "TAB",
-        'agvmgLtbOej09pGw27ZF' => "LAP",
-        'Z1Kx9vql4QxiMB9brOd2' => "i12", // my iPhone
-        'm2cNTf0BaASn5Z2rLrGa' => "Jana", // Jana's phone/computer?
-        'kSQmLodejUtIv65Ff7BF' => "SAMj320",
-        'aZxrc212BRsQLrMgyMZF' => "HPFox",
-        'FPc5lLUsrf46OK6mkste' => "SAMa10e",
-        'AP8Pbm3GSxShU92ozThM' => "BWindows", // bonnie's Windows Machien
-        'kh05wAVtBzacoRZD0ZUG' => "BiPhone",  // bonnie's iPhone
+        'hFBzuVRDeIWdbhXmhZv7' => "HP",      // HP using Chrome
+        'e30hJHxUeaToTAB6g4Zv' => "TAB",     // My tablet
+        'agvmgLtbOej09pGw27ZF' => "Inspiron",// My laptop
+        'Z1Kx9vql4QxiMB9brOd2' => "i12",     // my iPhone
+        'm2cNTf0BaASn5Z2rLrGa' => "Jana",    // Jana's phone/computer?
+        'kSQmLodejUtIv65Ff7BF' => "SAMj320", // Old Phone (android)
+        'FPc5lLUsrf46OK6mkste' => "SAMa10e", // New Phone (android)
+        'AP8Pbm3GSxShU92ozThM' => "BWindows",// bonnie's Windows Machine
+        'kh05wAVtBzacoRZD0ZUG' => "BiPhone", // bonnie's iPhone
+        'mUQurQMLPE4rk0mjhamh' => "HPFox",   // HP using Firefox
+        'Sf0RS3RfRhY8vke1a5uN' => "BIpad",   // Bonnie's iPad
+        'J4C3rOAXmbicnj3XFlGB' => "Acer",    // Acer Spin 3
        ];
